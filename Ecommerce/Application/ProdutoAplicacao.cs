a169e532f9e74db4bba3dd55e043f346c2f21ece
Ellen Santos <drummerandengineer_ellen@hotmail.com>
2021-05-12 21:57:04 -0300

    inclusao controller de produto

---
+++ Ecommerce/Application/ProdutoAplicacao.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ecommerce.Models;
+using Ecommerce.Context;
+
+namespace Ecommerce.Application
+{
+    public class ProdutoAplicacao
+    {
+        private INFONEWContext _contexto;
+
+        public ProdutoAplicacao(INFONEWContext contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public string InserirProduto(Produto prd)
+        {
+            try
+            {
+                if (prd != null)
+                {
+                    var produtoExiste = GetProdByCode(prd.CodProd);
+
+                    if (produtoExiste == null)
+                    {
+                        _contexto.Add(prd);
+                        _contexto.SaveChanges();
+
+                        return "Produto cadastrado com sucesso!";
+                    }
+                    else
+                    {
+                        return "produto já cadastrado na base de dados.";
+                    }
+                }
+                else
+                {
+                    return "Produto inválido!";
+                }
+            }
+            catch (Exception)
+            {
+                return "Não foi possível se comunicar com a base de dados!";
+            }
+        }
+
+        public string AtualizarProduto(Produto prd)
+        {
+            try
+            {
+                if (prd != null)
+                {
+                    _contexto.Update(prd);
+                    _contexto.SaveChanges();
+
+                    return "Produto alterado com sucesso!";
+                }
+                else
+                {
+                    return "Produto inválido!";
+                }
+            }
+            catch (Exception)
+            {
+                return "Não foi possível se comunicar com a base de dados!";
+            }
+        }
+
+        public Produto GetProdByCode(int codProd)
+        {
+            Produto produtoExistente = new Produto();
+
+            try
+            {
+                if (codProd == 0)
+                {
+                    return null;
+                }
+
+                var prd = _contexto.Produtos.Where(x => x.CodProd == codProd).ToList();
+                produtoExistente = prd.FirstOrDefault();
+
+                if (produtoExistente != null)
+                {
+                    return produtoExistente;                        ;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public List<Produto> GetAllProducts()
+        {
+            List<Produto> lstPrd = new List<Produto>();
+            try
+            {
+                foreach (var item in _contexto.Produtos)
+                {
+
+                    lstPrd.Add(item);
+                }
+
+
+                if (lstPrd != null)
+                {
+                    return lstPrd;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+        public string DeletePRodByCodProd(int codPRod)
+        {
+            try
+            {
+                if (codPRod == 0)
+                {
+                    return "Produto inválido! Por favor tente novamente.";
+                }
+                else
+                {
+                    var prd = GetProdByCode(codPRod);
+
+                    if (prd != null)
+                    {
+                        _contexto.Produtos.Remove(prd);
+                        _contexto.SaveChanges();
+
+                        return "Produto " + prd.NomeProd + " deletado com sucesso!";
+                    }
+                    else
+                    {
+                        return "Produto não cadastrado!";
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return "Não foi possível se comunicar com a base de dados!";
+            }
+        }
+    }
+}
---
+++ Ecommerce/Controllers/ProdutoController.cs
@@ -0,0 +1,98 @@
+﻿using Ecommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ecommerce.Application;
+using Ecommerce.Context;
+using Ecommerce.library;
+using System.Net;
+using System.Net.Http;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Ecommerce.Controllers
+{
+    [Route("fapen/produto")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+
+        private readonly INFONEWContext _context;
+
+        public ProdutoController(INFONEWContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public List<Produto> Get()
+        {
+            ProdutoAplicacao objAplicacao = new ProdutoAplicacao(_context);
+
+            try
+            {
+                return objAplicacao.GetAllProducts();
+            }
+            catch (Exception ex)
+            {
+                var retorno = new List<Produto>();
+                var prd = new Produto();
+                prd.NomeProd = ex.Message;
+                retorno.Add(prd);
+                return retorno;
+            }
+
+        }
+
+        [HttpGet("{CodProd}")]
+
+        public Produto Get(int CodProd)
+        {
+            ProdutoAplicacao objAplicacao = new ProdutoAplicacao(_context);
+            Produto prd = objAplicacao.GetProdByCode(CodProd);
+
+            return prd;
+        }
+
+
+        // PUT api/<ProdutoController>/5
+        [HttpPut()]
+        public string Put([FromBody] Produto value)
+        {
+            ProdutoAplicacao objAppProd = new ProdutoAplicacao(_context);
+
+            if (value != null)
+            {
+
+                if (value.CodProd > 0)
+                {
+                    string retorno = objAppProd.AtualizarProduto(value);
+                    return retorno;
+                }
+                else
+                {
+                    string retorno = objAppProd.InserirProduto(value);
+                    return retorno;
+                }
+            }
+            return "produto invalido!";
+
+        }
+
+        // DELETE api/<ProdutoController>/5
+        [HttpDelete("{CodProd}")]
+        public string Delete(int CodProd)
+        {
+            if (CodProd == 0)
+            {
+                return "produto inválido para exclusão";
+            }
+            ProdutoAplicacao objAppProd = new ProdutoAplicacao(_context);
+            string retorno = objAppProd.DeletePRodByCodProd(CodProd);
+
+            return retorno;
+        }
+    }
+}
